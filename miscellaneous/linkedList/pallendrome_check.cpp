cdea237f22bf9dfab9d5f1b05d2c15a6143cd4af
kajol <kajolkumarisingh222@gmail.com>
2020-10-28 22:06:11 +0530

    pallendrome_check

---
+++ miscellaneous/linkedList/pallendrome_check.cpp
@@ -0,0 +1,63 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l,firstdata;
+    cin>>T;
+    while(T--)
+    {
+        
+        struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        // taking first data of LL
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        // taking remaining data of LL
+        for(i=1;i<n;i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+bool isPalindrome(Node *head)
+{
+    stack<int>s;
+    Node* h = head;
+    while(h){
+        s.push(h->data);
+        h = h -> next;
+    }
+    while(!s.empty() && head){
+        int d = s.top();
+        s.pop();
+        if(d != head->data){
+            return false;
+        } 
+        head = head ->next;
+    }
+    return true;
+}
