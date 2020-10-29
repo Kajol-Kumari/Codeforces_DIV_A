804d2c37ac4a64ca1419feefee3799e2e82fe0e1
kajol <kajolkumarisingh222@gmail.com>
2020-10-29 20:58:11 +0530

    queue_using_listed_list

---
+++ miscellaneous/linkedList/queue_using_linked_list.cpp
@@ -0,0 +1,109 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+/* The method push to push element into the queue*/
+void MyQueue:: push(int x)
+{
+    QueueNode* newNode = new QueueNode(x);
+    if(front == NULL && rear == NULL){
+        front = rear = newNode;
+        rear->next = NULL;
+    } else {
+        rear->next = newNode;
+        rear = rear->next;
+        rear->next = NULL;
+    }
+}
+
+/*The method pop which return the element
+  poped out of the queue*/
+int MyQueue :: pop()
+{
+    if(front == NULL && rear == NULL){
+        return -1;
+    } else if(front->next == NULL){
+        QueueNode* temp = front;
+        int d= temp->data;
+        front = rear = NULL;
+        delete(temp);
+        return d;
+    } else {
+        QueueNode* temp = front;
+        int d= temp->data;
+        front = front->next;
+        delete(temp);
+        return d;
+    }       
+}
